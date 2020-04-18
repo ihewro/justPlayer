b2a8a135ebfc2242af6da12b3e0b2f16cebe6751
hewro <ihewro@163.com>
2020-04-18 19:22:38 +0800

    init repo

---
+++ cmake-build-debug/CMakeFiles/justPlayer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/justPlayer.dir/main.cpp.o"
+  "justPlayer"
+  "justPlayer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/justPlayer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
